b7c7047a2ad4167a5928ce10744ddcc6f54ae422
Sergey K <r081n4k@gmail.com>
2022-01-10 23:17:24 +0300

    create bot

---
+++ go.mod
@@ -0,0 +1,3 @@
+module echo-telegram-bot
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+// entry point
+func main() {
+	botToken := os.Getenv("botToken")
+	botApi := "https://api.telegram.org/bot"
+	botUrl := botApi + botToken
+	offset := 0
+	for {
+		updates, err := getUpdates(botUrl, offset)
+		if err != nil {
+			log.Println("Smth went wrong: ", err.Error())
+		}
+		for _, update := range updates {
+			err = respond(botUrl, update)
+			offset = update.UpdateId + 1
+		}
+		fmt.Println(updates)
+	}
+}
+
+// request updates
+func getUpdates(botUrl string, offset int) ([]Update, error) {
+	resp, err := http.Get(botUrl + "/getUpdates" + "?offset=" + strconv.Itoa(offset))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var restResponse RestResponse
+	err = json.Unmarshal(body, &restResponse)
+	if err != nil {
+		return nil, err
+	}
+	return restResponse.Result, nil
+}
+
+// echo func
+func respond(botUrl string, update Update) error {
+	var botMessage BotMessage
+	botMessage.ChatId = update.Message.Chat.ChatId
+	botMessage.Text = update.Message.Text
+	buf, err := json.Marshal(botMessage)
+	if err != nil {
+		return err
+	}
+	_, err = http.Post(botUrl+"/sendMessage", "application/json", bytes.NewBuffer(buf))
+	if err != nil {
+		return err
+	}
+	return nil
+}
